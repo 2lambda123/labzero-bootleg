4884ee9585a482d02820e9c0f95a86764950ae7d
Restyled.io <commits@restyled.io>
2024-08-17 19:18:52 +0000

    Restyled by shellcheck

--- script/ci/prepare.sh
+++ script/ci/prepare.sh
@@ -12,14 +12,14 @@ export ELIXIR_VERSION="v1.6.6"
 export ERLANG_PATH="$INSTALL_PATH/otp_src_$ERLANG_VERSION"
 export ELIXIR_PATH="$INSTALL_PATH/elixir_$ELIXIR_VERSION"
 
-mkdir -p $INSTALL_PATH
-cd $INSTALL_PATH
+mkdir -p "$INSTALL_PATH"
+cd "$INSTALL_PATH"
 
 # Install erlang
-if [ ! -e $INSTALL_PATH/bin/erl ]; then
+if [ ! -e "$INSTALL_PATH"/bin/erl ]; then
   curl -L -O http://www.erlang.org/download/otp_src_$ERLANG_VERSION.tar.gz
   tar xzf otp_src_$ERLANG_VERSION.tar.gz
-  cd $ERLANG_PATH
+  cd "$ERLANG_PATH"
   ./configure --enable-smp-support \
               --enable-m64-build \
               --disable-native-libs \
@@ -28,35 +28,35 @@ if [ ! -e $INSTALL_PATH/bin/erl ]; then
               --enable-kernel-poll \
               --disable-hipe \
               --without-javac \
-              --prefix=$INSTALL_PATH
+              --prefix="$INSTALL_PATH"
   make install
 else
   echo "Erlang already installed."
 fi
 
 # Install elixir
-if [ ! -e $INSTALL_PATH/bin/elixir ]; then
-  git clone https://github.com/elixir-lang/elixir $ELIXIR_PATH
-  cd $ELIXIR_PATH
+if [ ! -e "$INSTALL_PATH"/bin/elixir ]; then
+  git clone https://github.com/elixir-lang/elixir "$ELIXIR_PATH"
+  cd "$ELIXIR_PATH"
   git checkout $ELIXIR_VERSION
   PREFIX=$INSTALL_PATH make install
 else
   echo "Elixir already installed."
 fi
 
-if [ $VERSION_CIRCLECI -ne 2 ]; then
+if [ "$VERSION_CIRCLECI" -ne 2 ]; then
   # Install package tools
   mix help | grep [^.]hex > /dev/null
   if [ $? -ne 0 ]; then
     yes Y | LC_ALL=en_GB.UTF-8 mix local.hex
   fi
 
-  if [ ! -e $HOME/.mix/rebar ]; then
+  if [ ! -e "$HOME"/.mix/rebar ]; then
     yes Y | LC_ALL=en_GB.UTF-8 mix local.rebar
   fi
 
   # Fetch and compile dependencies and application code (and include testing tools)
   export MIX_ENV="test"
-  cd $HOME/$CIRCLE_PROJECT_REPONAME
+  cd "$HOME"/"$CIRCLE_PROJECT_REPONAME"
   mix do deps.get, deps.compile, compile, dialyzer --plt
 fi
