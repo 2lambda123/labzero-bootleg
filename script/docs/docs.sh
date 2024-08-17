75094e6f29101b61b2739cdf3026bae2eb8ed381
Restyled.io <commits@restyled.io>
2024-08-17 19:19:23 +0000

    Restyled by shellharden

--- script/docs/docs.sh
+++ script/docs/docs.sh
@@ -5,7 +5,7 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 echo "$SCRIPT_DIR"
 
-cd "${SCRIPT_DIR}/../.."
+cd "$SCRIPT_DIR/../.."
 
 if ! command -v pip3 >/dev/null; then
     echo "You must have Python and Pip installed to build the docs!"
