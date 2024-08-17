a8a3d21a700f4f2c6ef8d85176db8d71da6c4b90
Restyled.io <commits@restyled.io>
2024-08-17 19:19:23 +0000

    Restyled by shfmt

--- script/docs/docs.sh
+++ script/docs/docs.sh
@@ -8,17 +8,17 @@ echo "$SCRIPT_DIR"
 cd "$SCRIPT_DIR/../.."
 
 if ! command -v pip3 >/dev/null; then
-    echo "You must have Python and Pip installed to build the docs!"
-    exit 2
+  echo "You must have Python and Pip installed to build the docs!"
+  exit 2
 fi
 
 if ! command -v virtualenv >/dev/null; then
-    echo "Need virtualenv, but not present, installing.."
-    pip3 install virtualenv
+  echo "Need virtualenv, but not present, installing.."
+  pip3 install virtualenv
 fi
 
 if [ ! -d _venv ]; then
-    virtualenv _venv
+  virtualenv _venv
 fi
 source _venv/bin/activate
 pip3 install -q -r script/docs/requirements.txt
